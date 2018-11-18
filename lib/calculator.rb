97e21689509ff7c32604fe15b4ad1506594cd759
PersonofNote <jtm89@nau.edu>
2018-11-18 10:52:23 -0800

    Added subtract, multiply and divide methods and tests to round it out

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,9 +1,23 @@
 class Calculator
 	def add(*args)
-    	@total=0
+    	@total = 0
 		args.each do |num|
     	@total += num
     	end
     	return @total
+	end
+	def subtract(arg, *args)
+		@total = arg
+		args.each do |num|
+		@total -= num
+	end
+	return @total
+	end
+	def multiply(arg, *args)
+		@total = arg
+		args.each do |num|
+		@total *= num
+	end
+	return @total
 	end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -12,4 +12,24 @@ RSpec.describe Calculator do
     	expect(calculator.add(2,5,7)).to eql(14)
     end
  	end
+ 	describe "#subtract" do
+    it "returns the difference of two numbers" do
+      calculator = Calculator.new
+      expect(calculator.subtract(5, 2)).to eql(3)
+    end
+    it "returns the difference of more than two numbers" do
+    	calculator = Calculator.new
+    	expect(calculator.subtract(5,2,1)).to eql(2)
+    end
+ 	end
+ 	describe "#multiply" do
+    it "returns the product of two numbers" do
+      calculator = Calculator.new
+      expect(calculator.multiply(5, 2)).to eql(10)
+    end
+    it "returns the product of more than two numbers" do
+    	calculator = Calculator.new
+    	expect(calculator.multiply(5,2,3)).to eql(30)
+    end
+ 	end
 end
